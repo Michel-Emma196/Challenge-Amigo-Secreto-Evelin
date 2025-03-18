0abacb221b5575ae1701b81d3106d74b27eec543
Michel-Emma196 <evelinmorarro@gmail.com>
2025-03-18 18:09:29 -0500

    Update app.js
    implementacióm de la función

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear un array para almacenar los nombres y Implementa una función para agregar amigos
